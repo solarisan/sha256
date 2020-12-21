5052fa8274feebd82d17b063a463b00321e6913c
b1859680@trbvn.com <b1859680@trbvn.com>
2020-12-21 17:04:20 -0500

    adding time counter

--- sha256.cpp
+++ sha256.cpp
@@ -6,6 +6,7 @@
 #include <sys/stat.h>
 #include <sstream>
 #include <iomanip>
+#include <time.h>
 
 using namespace std;
 
@@ -64,19 +65,11 @@ std::uint32_t majority(std::uint32_t* x, std::uint32_t* y, std::uint32_t* z){
     }
     return static_cast<std::uint32_t>(v.to_ulong());
 }
-
-int main(){
-
-    //snippet to give a file size////////////////////////////////////
-    struct stat results;  
-    if (stat("input.log", &results) == 0){
-        //cout << (results.st_size) << endl; //gives the size of the file in bytes (note if the file is ascii every new line equals two bytes)
-    }else{
-        cout<<"error in reading file"<<endl;  // An error occurred   
-    }
-
     //constant values for SHA256   ////////////////////////////////////////////////////////////
-    std::vector<std::uint32_t> k{   0x428a2f98,    0x71374491,    0xb5c0fbcf,    0xe9b5dba5,
+
+const std::vector<std::uint32_t> inital{  0x6a09e667,    0xbb67ae85,    0x3c6ef372,    0xa54ff53a,
+                                        0x510e527f,    0x9b05688c,    0x1f83d9ab,    0x5be0cd19}; 
+const std::vector<std::uint32_t> k{   0x428a2f98,    0x71374491,    0xb5c0fbcf,    0xe9b5dba5,
                                     0x3956c25b,    0x59f111f1,    0x923f82a4,    0xab1c5ed5,
                                     0xd807aa98,    0x12835b01,    0x243185be,    0x550c7dc3,
                                     0x72be5d74,    0x80deb1fe,    0x9bdc06a7,    0xc19bf174,
@@ -92,27 +85,29 @@ int main(){
                                     0x391c0cb3,    0x4ed8aa4a,    0x5b9cca4f,    0x682e6ff3,
                                     0x748f82ee,    0x78a5636f,    0x84c87814,    0x8cc70208,
                                     0x90befffa,    0xa4506ceb,    0xbef9a3f7,    0xc67178f2}; 
-
-    std::vector<std::uint32_t> inital{  0x6a09e667,    0xbb67ae85,    0x3c6ef372,    0xa54ff53a,
-                                        0x510e527f,    0x9b05688c,    0x1f83d9ab,    0x5be0cd19}; 
+void calculator(const char *nonce){
 
     std::vector<std::uint32_t> compress{0x6a09e667,    0xbb67ae85,    0x3c6ef372,    0xa54ff53a,
                                         0x510e527f,    0x9b05688c,    0x1f83d9ab,    0x5be0cd19}; 
 
-    const char *str="";
+    const char *str = ""+(*nonce);
+    
+    // std::string buf(str);
+    // buf.append(nonce);
+
     int stringLen=TextToBinaryString(str).size();   //size of string in bits
     int padding=512-(stringLen+1+64)%512;  //pading size of zeros to make chunks of 512 bits
 
-    cout<<"The string to be hashed is:\t\t"<<TextToBinaryString(str)<<endl;
-    cout<<"The size of the string in bits: \t"<<stringLen<<endl;
-    cout<<"how many 0 bits for padding needed:\t"<<padding<<endl;
+    // cout<<"The string to be hashed is:\t\t"<<TextToBinaryString(str)<<endl;
+    // cout<<"The size of the string in bits: \t"<<stringLen<<endl;
+    // cout<<"how many 0 bits for padding needed:\t"<<padding<<endl;
 
     bitset<512> workBitset{0x0};
     workBitset=workBitset|bitset<512>(TextToBinaryString(str));
     workBitset=workBitset<<(512-stringLen);
     workBitset=workBitset.flip(512-stringLen-1);
     workBitset=workBitset^bitset<512>(stringLen);
-    cout<<workBitset<<endl;
+    // cout<<workBitset<<endl;
 
     vector<std::uint32_t> wordsSchedule;
     string stringWorkSet=workBitset.to_string();
@@ -125,11 +120,11 @@ int main(){
         }
     }
 
-    cout << "\n\n word size before modification: " << wordsSchedule.size() << "\n\n";
-    for (int item=0;item<16;item++){
-        cout << bitset<32>(wordsSchedule.at(item)) << endl;
-    }
-    cout<<"\n-------------------- word schedule 64 words:\n";
+    // cout << "\n\n word size before modification: " << wordsSchedule.size() << "\n\n";
+    // for (int item=0;item<16;item++){
+    //     cout << bitset<32>(wordsSchedule.at(item)) << endl;
+    // }
+    //cout<<"\n-------------------- word schedule 64 words:\n";
     for(int w=16; w<64; w++){
         std::uint32_t w2=wordsSchedule.at(w-2);
         std::uint32_t w7=wordsSchedule.at(w-7);
@@ -141,15 +136,15 @@ int main(){
         wordsSchedule.push_back(w2+w7+w15+w16);
     }
 
-    for (int item=0; item<64; item++){
-        cout << bitset<32>(wordsSchedule.at(item)) << endl;
-    }
+    // for (int item=0; item<64; item++){
+    //     cout << bitset<32>(wordsSchedule.at(item)) << endl;
+    // }
 
     for(int w=0; w<64; w++){
-        cout << "\n\n--------------- in step: " << w << endl;
-        for(int z=0; z<8; z++){   
-            cout << bitset<32>(compress.at(z)) << endl;
-        }
+        // cout << "\n\n--------------- in step: " << w << endl;
+        // for(int z=0; z<8; z++){   
+        //     cout << bitset<32>(compress.at(z)) << endl;
+        // }
         std::uint32_t working0=wordsSchedule.at(w);
         std::uint32_t key0=k.at(w);
         std::uint32_t zero=0x0;
@@ -170,26 +165,45 @@ int main(){
         compress.at(0)=task1+task2;
         compress.at(4)+=task1;
 
-        cout << "\n\n task 1+2:\t" << bitset<32>(task1+task2) << "\n task1:\t\t" << bitset<32>(task1) << endl;
+        // cout << "\n\n task 1+2:\t" << bitset<32>(task1+task2) << "\n task1:\t\t" << bitset<32>(task1) << endl;
     }
     
     for(int sum=0; sum<8; sum++){   
         compress.at(sum)+=inital.at(sum); 
     }
 
-    cout<<"\n\n the final hash:-----------------------------"<<endl;
-    for(int z=0; z<8; z++){   
-        cout << bitset<32>(compress.at(z)) << endl;
-    }
+    // cout<<"\n\n the final hash:-----------------------------"<<endl;
+    // for(int z=0; z<8; z++){   
+    //     cout << bitset<32>(compress.at(z)) << endl;
+    // }
 
-    cout<<"\n\nconverting to hex-----------------------------"<<endl;
+    // cout<<"\n\nconverting to hex-----------------------------"<<endl;
 
     for(int z=0; z<8; z++){        
-        cout << uppercase << setw(sizeof(std::uint32_t)*2) << std::hex << compress.at(z) << endl;       
+        cout << uppercase << setw(sizeof(std::uint32_t)*2) << std::hex << compress.at(z);       
     }   
+    cout <<endl;
 
+
+}
+
+int main(){
+    clock_t tStart = clock();
+    //snippet to give a file size////////////////////////////////////
+    struct stat results;  
+    if (stat("input.log", &results) == 0){
+        //cout << (results.st_size) << endl; //gives the size of the file in bytes (note if the file is ascii every new line equals two bytes)
+    }else{
+        cout<<"error in reading file"<<endl;  // An error occurred   
+    }
+
+    for (int x=0; x<10000;x++){
+    const char *nonce="hello john smith hope you are doing well";
+    calculator(nonce);
+
+    }
     //myb=(myb+1) % 4294967296; //modulu 2**32
-
+    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
     return 0;
 }
 
